13b6ef8efa878e363abfce69aeadf084e572e0da
sergiovzambelli <sergiovzambelli@gmail.com>
2025-04-29 11:37:43 +0200

    Feature: added PIVA

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
 const Footer = () => (
-  <footer className="pt-12 pb-16 mt-16 text-center sm:pt-8 sm:pb-8 sm:mt-12">
+  <footer className="pt-12 pb-16 mt-16 text-center sm:pt-8 sm:pb-8 sm:mt-12 flex flex-col">
     <a
       href="https://github.com/sergiovzambelli"
       className="text-primary font-bold text-sm hover:underline"
     >
       Created with love By Sergio Vittorio Zambelli
     </a>
+    <div className="text-sm">
+      P.IVA: 13521030968
+    </div>
   </footer>
 );
 
